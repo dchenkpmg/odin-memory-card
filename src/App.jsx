a58d28a1a87700ef4eeaf8c7a21d7aec59f89f13
dchenkpmg <dylanchen1@kpmg.co.nz>
2025-03-19 01:23:07 +1300

    feat: add pokemon fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=8"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let data = await response.json();
+
+        const pokemon = data.results.map(async (pokemon) => {
+          const pokeResponse = await fetch(pokemon.url);
+          if (!pokeResponse.ok) {
+            throw new Error(`HTTP error: Status ${pokeResponse.status}`);
+          }
+          return pokeResponse.json();
+        });
+
+        const allPokemon = await Promise.all(pokemon);
+        setCards(allPokemon);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setCards(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCards();
+  }, []);
+
+  console.log(cards);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <title>Memory Game</title>
+      <header>
+        <h1>Memory Game</h1>
+      </header>
+      <main>
+        <div className="card-grid">
+          {loading && <p>Loading Pokemon...</p>}
+          {error && <p>{error}</p>}
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
